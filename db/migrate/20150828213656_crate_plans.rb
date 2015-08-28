920509e7f6c0e83fc48f0dc19a9a7cd1b4ff4329
George Mey <gmeyer1000@yahoo.com>
2015-08-28 22:05:41 +0000

    add associtations with plans and users

---
+++ db/migrate/20150828213656_crate_plans.rb
@@ -0,0 +1,11 @@
+class CratePlans < ActiveRecord::Migration
+  def change
+    create_table :plans do |t|
+      t.string :name
+      t.decimal :price
+      
+      t.timestamps 
+    
+    end
+  end
+end
