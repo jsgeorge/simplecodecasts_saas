83ae2017d07a6e0650cde10b3e3d56d136f3a489
George Mey <gmeyer1000@yahoo.com>
2015-09-02 00:02:45 +0000

    Added noteice and styles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-
+    
+    before_filter :select_plan, only: :new
+    
     def create
         super do |resource|
             if params[:plan]
@@ -11,6 +13,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
                  end
             end
         end
+    end
+    
+    private
+        def select_plan
+         unless params[:plan] && (params[:plan]== '1' || params[:plan] == '4')
+                flash[:notice] = "please select a membership lplan to sign up."
+                redirect_to root_url
+         end
     end
     
 end
