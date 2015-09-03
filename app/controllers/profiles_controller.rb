5d2ffc30401e3cc6ab114396b3c3daf65a5c3944
George Mey <gmeyer1000@yahoo.com>
2015-09-03 16:41:47 +0000

    reainstalled user porofiels

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,7 @@
 class ProfilesController < ApplicationController
-    
+    def new
+        #each user fills out own form
+        @user = User.find(params[:user_id])
+        @profile = @user.build_profile
+    end
 end
